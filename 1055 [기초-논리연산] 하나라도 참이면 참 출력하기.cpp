5f884bd0f8c559e154cdb9a1a2bf102069141043
The-Shinee <68369195+The-Shinee@users.noreply.github.com>
2020-12-07 02:39:03 +0900

    Correct: 1055
    코드업 1055번 정답 소스코드

--- 1055 [기초-논리연산] 하나라도 참이면 참 출력하기.cpp
+++ 1055 [기초-논리연산] 하나라도 참이면 참 출력하기.cpp
@@ -2,24 +2,10 @@
 
 int main()
 {
-	try
-	{
-		int a, b;
+	int a, b;
 		
-		std::cin >> a >> b;
-		
-		if(a != 0 && a != 1)
-			throw a;
-		if(b != 0 && b != 1)
-			throw b;
-			
-		std::cout << (a || b);
-	}
-	catch(int expn)
-	{
-		std::cout << "[ error ] : a �Ǵ� b ���� ������ ������ϴ�." << std::endl;
-		std::cout << "��� �� : " << expn << std::endl; 
-	}
-	
+	std::cin >> a >> b;
+	std::cout << (a || b);
+
 	return 0;
 }
