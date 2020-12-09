8fc85080da04f6493774b2771b9bddf1f188778f
The-Shinee <68369195+The-Shinee@users.noreply.github.com>
2020-12-09 23:16:13 +0900

    Correct: 1133
    코드업 1133번 정답 소스코드

---
+++ 1133 공백이 있는 문자열 입출력.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    
+    char str[31];
+
+    cin.getline(str, 31);
+    cout << str;
+
+    return 0;
+}
